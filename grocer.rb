385636a288286af275ff2470767e012aee03d313
Scoti Dodson <scotidodson@gmail.com>
2018-09-13 05:32:35 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,46 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
-end
+  cart_hash = {}
+  cart.each do |cart_items|
+    cart_items.each do |item, item_info|   
+      if cart_hash[item].nil?
+        cart_hash[item] = item_info.merge({:count => 1})
+      else
+        cart_hash[item][:count] += 1 
+      end
+    end 
+  end 
+  cart_hash
+end 
+
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coup|
+    item = coup[:item]
+    if cart[item] && cart[item][:count] >= coup[:num]
+      if cart["#{item} W/COUPON"]
+        cart["#{item} W/COUPON"][:count] += 1
+      else
+        cart["#{item} W/COUPON"] = {:count => 1, :price => coup[:cost]}
+        cart["#{item} W/COUPON"][:clearance] = cart[item][:clearance]
+      end
+      cart[item][:count] -= coup[:num]
+    end
+  end
+  cart
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, item_info|
+    if cart[item][:clearance] == true 
+      x = cart[item][:price] * 0.80
+      cart[item][:price] = x.round(2)
+    end
+  end 
+  cart 
 end
 
 def checkout(cart, coupons)
-  # code here
+
 end
